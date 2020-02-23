e3ec32c200eba209f2604d2f943001833d4964ad
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2020-02-22 19:10:18 -0500

    I add a test of a Grid System

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 //Welcome to QuarzoJS
 import ResponsiveNavbar from './modules/ResponsiveNavbar';
+import Grid from './modules/Grid';
 //Styles
 import './css/style.css';
 import './css/ResponsiveNavbar.css';
+
+//Setting the main grid
+const mainGridContainer = document.getElementById('main'); //We take the container of the grid
+
+//Creating the grid
+const mainGrid = new Grid(mainGridContainer, {
+    gap: '20px'
+}, []);
+
+console.log(mainGrid.getElement('main').style.gridTemplateAreas);
+
+//Ends the Grid
 
 //Here we create the main menu :-)
 const mainMenu = new ResponsiveNavbar({
     elementId: 'navbar',
     fixed: true,
     height: '60px',
-    color: 'black',
+    color: '#2c2c2c',
     links: [
         {name: 'Home', url: '#'},
         {name: 'About us', url: '#'},
@@ -23,3 +36,21 @@ const mainMenu = new ResponsiveNavbar({
     colorSubMenu: '#eee'
 });
 //Ends the navbar
+
+//We create the Row for push navbar
+
+mainGrid.createRow([
+    {content: mainMenu.getElement('navbar'), numberColumns: 12, area: "navbar"}
+]);
+//Ends creating the row for navbar
+
+//Test of other double row
+mainGrid.createRow([
+    {content: document.getElementById('uno'), numberColumns: 6, area: "rojo"},
+    {content: document.getElementById('dos'), numberColumns: 6, area: "amarillo"}
+]);
+
+
+mainGrid.createRow([
+    {content: document.getElementById('tres'), numberColumns: 12, area: "azul"}
+]);
